befcd83dd759984ec8ee1902acea15419f923d13
Miguel Serrano <miguelvps@gmail.com>
2019-09-23 16:15:40 +0100

    DTR-111 Add user friendly message when error code is 3

--- src/Events.js
+++ src/Events.js
@@ -230,7 +230,7 @@ function withEvents(BCPlayerComponent) {
 				 * Error Code that indicates there was an error returned by the API. It could be any error from the API.
 				 */
 				if (error_code === '3') {
-					error_code = 'PLAYER_ERROR';
+					return { errorCode = 'PLAYER_ERROR', errorMessage: 'There was an error with the server. Please try again.' + ((errorMessage) ? `(${errorMessage})` : '') };
 				}
 			}
 

4ee198f61d1840f118463d1c4a7b3fc699b09ef6
vikasd11 <vikas.m@fancode.com>
2019-09-24 17:06:32 +0530

    feat(videoOverlay): added screen toggle button

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const BCPlayer = require('./src/BCPlayer');
 const ScrollView = require('./src/ScrollView');
 const PlayerEventTypes = require('./src/PlayerEventTypes');
 const ControlBar = require('./src/ControlBar');
+const ToggleIcon = require('./src/ToggleIcon');
+const Scrubber = require('./src/Scrubber');
+const Time = require('./src/Time');
 
 module.exports = {
+    Time: Time,
+    Scrubber: Scrubber,
+    ToggleIcon: ToggleIcon,
     ControlBar: ControlBar,
     BCPlayer: BCPlayer,
     BrightcovePlayer: BrightcovePlayer,
